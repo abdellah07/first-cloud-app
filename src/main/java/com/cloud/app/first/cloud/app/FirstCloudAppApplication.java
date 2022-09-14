eb45db7d03220dfe948454b637d9c87a99a3637e
abdellah <abdellah.naji@etu.unice.fr>
2022-09-14 11:22:16 +0200

    first commit

---
+++ src/main/java/com/cloud/app/first/cloud/app/FirstCloudAppApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.app.first.cloud.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstCloudAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud/app/first/cloud/app/FirstCloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.app.first.cloud.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstCloudAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
